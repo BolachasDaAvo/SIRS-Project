25fd9f34bd6dc25bf9068ce0ab9181a2c8fc8f31
Katherine Fernandes <39100068+katherinefernandes@users.noreply.github.com>
2020-12-07 17:59:03 +0000

    command line working

--- client/src/main/java/sirs/client/ClientApp.java
+++ client/src/main/java/sirs/client/ClientApp.java
@@ -12,34 +12,39 @@ public class ClientApp {
 
         ClientLogic logic = new ClientLogic(host, port);
 
-/*         logic.register("test", "keys/cert.pem");
-        logic.register("test2", "keys/cert.pem");
-        logic.login("test", "keys/key_pkcs8.key");
         try {
-            logic.upload(args[0]);
-        } catch (Exception e) {
-            e.printStackTrace();
-            logic.close();
-            return;
-        } */
 
-/*         try {
-            logic.invite(args[0], args[1]);
-        } catch (Exception e) {
-            e.printStackTrace();
-            logic.close();
-            return;
-        } */
+            switch (args[0]) {
+                case "login":
+                    logic.login(args[1], "keys/key_pkcs8.key");
+                    break;
 
-        try {
-            logic.login("test2", "keys/key_pkcs8.key");
-            logic.accept(args[0]);
-            logic.download(args[0]);
+                case "download":
+                    logic.download(args[1]);
+                    break;
+
+                case "upload":
+                    logic.upload(args[1]);
+                    break;
+
+                case "invite":
+                    logic.invite(args[1], args[2]);
+                    break;
+
+                case "register":
+                    logic.register(args[1], "keys/cert.pem");
+                    break;
+
+                default:
+                    System.out.println("Invalid command, try again");
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
             logic.close();
             return;
         }
+
 
         logic.close();
         System.out.println("bye!");
